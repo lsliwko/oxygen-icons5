aaec070d6683cbc37e3f0474a33c2247cac3b958
Leszek Sliwko <leszek.sliwko@digital.homeoffice.gov.uk>
2022-04-28 23:59:41 +0100

    more icons

--- src/main/resources/thumbnails/convert-scalable.sh
+++ src/main/resources/thumbnails/convert-scalable.sh
@@ -2,10 +2,10 @@
 
 convert_scalable_for_size() {
     SIZE=${1}
-    TARGET_PATH=`echo ./${SIZE}x${SIZE}/mimetypes/`
+    TARGET_PATH=`echo ./${SIZE}x${SIZE}/${2}/`
     mkdir -p ${TARGET_PATH}
 
-    for svgfile in $(ls ./scalable/*.svgz); do
+    for svgfile in $(ls ./scalable/${2}/*.svgz); do
         pngfile=`basename ${svgfile} | sed 's/svgz$/png/g'`
         jpgfile=`basename ${svgfile} | sed 's/svgz$/jpg/g'`
 
@@ -13,14 +13,27 @@ convert_scalable_for_size() {
         rsvg-convert -h ${SIZE} -f png $svgfile > ${TARGET_PATH}$pngfile
         convert -background white -alpha remove -alpha off ${TARGET_PATH}$pngfile ${TARGET_PATH}$jpgfile
     done
+
+    for svgfile in $(ls ./scalable/${2}/*.svg); do
+        pngfile=`basename ${svgfile} | sed 's/svg$/png/g'`
+        jpgfile=`basename ${svgfile} | sed 's/svg$/jpg/g'`
+
+        echo "Processing $svgfile into png and jpg with size ${SIZE}"
+        rsvg-convert -h ${SIZE} -f png $svgfile > ${TARGET_PATH}$pngfile
+        convert -background white -alpha remove -alpha off ${TARGET_PATH}$pngfile ${TARGET_PATH}$jpgfile
+    done
 }
 
-convert_scalable_for_size 16
-convert_scalable_for_size 22
-convert_scalable_for_size 32
-convert_scalable_for_size 48
-convert_scalable_for_size 64
-convert_scalable_for_size 96
-convert_scalable_for_size 128
-convert_scalable_for_size 256
-convert_scalable_for_size 512
+for value in actions applets apps categories devices emblems emotes mimetypes places status
+do
+    echo "Converting $value..."
+    convert_scalable_for_size 16 $value
+    convert_scalable_for_size 22 $value
+    convert_scalable_for_size 32 $value
+    convert_scalable_for_size 48 $value
+    convert_scalable_for_size 64 $value
+    convert_scalable_for_size 96 $value
+    convert_scalable_for_size 128 $value
+    convert_scalable_for_size 256 $value
+    convert_scalable_for_size 512 $value
+done
